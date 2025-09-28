cf80e6a488554c9cb34362b0d738674161bc3b44
Saifulla Shariff <saifullashariff2000@gmail.com>
2025-09-28 18:50:05 +0530

    Add all project files and folders

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,12 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
+  },
+  base: '/',
+  server: {
+    host: true
+  }
 });
